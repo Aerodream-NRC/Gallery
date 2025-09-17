234821537d0edaa16ef05bd70ff23dc1f989e1f1
Aerodream <nrc.aerodream@gmail.com>
2025-09-17 19:19:07 +0300

    Добавил артворксервис

---
+++ src/main/java/com/aerodream/Gallery/Service/ArtworkService.java
@@ -0,0 +1,15 @@
+package com.aerodream.Gallery.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class ArtworkService {
+
+
+}
