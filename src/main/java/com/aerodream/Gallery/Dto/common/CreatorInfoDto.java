65c6daf70632694dc97e8bf7abe92a7229023c50
Aerodream <nrc.aerodream@gmail.com>
2025-09-19 02:14:38 +0300

    Добавил аннотации из ломбока

--- src/main/java/com/aerodream/Gallery/Dto/common/CreatorInfoDto.java
+++ src/main/java/com/aerodream/Gallery/Dto/common/CreatorInfoDto.java
@@ -1,4 +1,14 @@
 package com.aerodream.Gallery.Dto.common;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreatorInfoDto {
+
 }
